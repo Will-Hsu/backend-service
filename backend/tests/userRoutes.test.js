197c24c117f75aff0ba89d4204cfecc6235b52b7
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2024-05-01 14:25:46 -0700

    Update Jest Unit Test to ensure functional coverage

--- backend/tests/userRoutes.test.js
+++ backend/tests/userRoutes.test.js
@@ -22,7 +22,7 @@ describe("User Routes", () => {
     await mongoServer.stop();
   });
 
-  it("should register a new user", async () => {
+  it("should REGISTER a new user", async () => {
     const userData = {
       name: "Maggie Moradi",
       password: "password123",
@@ -35,7 +35,22 @@ describe("User Routes", () => {
     expect(response.body.message).toBe("User registered successfully");
   });
 
-  it("should not register a duplicate user", async () => {
+  it("should not REGISTER an empty user", async () => {
+    const userData = {
+      name: "",
+      password: "password123",
+      email: "maggie@intuit.com",
+    };
+
+    const response = await request(app).post("/user/register").send(userData);
+
+    expect(response.status).toBe(400);
+    expect(JSON.parse(response.error.text).error).toBe(
+      "Please Provide Non Empty User Information for Registration!"
+    );
+  });
+
+  it("should not REGISTER a duplicate user", async () => {
     const userData = {
       name: "Maggie Moradi",
       password: "password123",
@@ -48,7 +63,7 @@ describe("User Routes", () => {
     expect(JSON.parse(response.error.text).error).toBe("User already exists");
   });
 
-  it("should log in an existing user", async () => {
+  it("should LOGIN an existing user", async () => {
     const credentials = {
       email: "maggie@intuit.com",
       password: "password123",
@@ -60,7 +75,32 @@ describe("User Routes", () => {
     expect(response.body.message).toBe("Login successful");
   });
 
-  it("should not log in with in correct credentials", async () => {
+  it("should not LOGIN with an empty user", async () => {
+    const credentials = {
+      email: "",
+      password: "password123",
+    };
+
+    const response = await request(app).post("/user/login").send(credentials);
+
+    expect(response.status).toBe(400);
+    expect(JSON.parse(response.error.text).error).toBe(
+      "Please Provide Non Empty User Information for Login!"
+    );
+  });
+
+  it("should not LOGIN with a non-existing user", async () => {
+    const credentials = {
+      email: "will@ucsd.com",
+      password: "password123",
+    };
+    const response = await request(app).post("/user/login").send(credentials);
+
+    expect(response.status).toBe(401);
+    expect(JSON.parse(response.error.text).error).toBe("User does not exist");
+  });
+
+  it("should not LOGIN with in correct credentials", async () => {
     const credentials = {
       email: "maggie@intuit.com",
       password: "wrongpassword",
